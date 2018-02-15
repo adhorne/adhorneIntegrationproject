fa91631ce419ad5a72d1dba7d0217799d943d490
adhorne <34281624+adhorne@users.noreply.github.com>
2018-02-15 17:03:33 -0500

    Update Main.java

--- Workspace/adhorneIntegration/src/adhorneIntegration/Main.java
+++ Workspace/adhorneIntegration/src/adhorneIntegration/Main.java
@@ -131,15 +131,5 @@ public class Main {
 
 
 
-class Bicycle {
-  private int speed;
-  private int cadence;
 
-  public int getSpeed() {
-    return speed;
-  }
 
-  public void setSpeed(int s) {
-    speed = s;
-  }
-}
