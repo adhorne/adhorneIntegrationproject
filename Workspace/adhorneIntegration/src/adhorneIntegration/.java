4e70ba0d5c9db81ca04bcc1e37a0eb02a15a558f
adhorne <34281624+adhorne@users.noreply.github.com>
2018-03-14 18:42:53 -0400

    Create .java

---
+++ Workspace/adhorneIntegration/src/adhorneIntegration/.java
@@ -0,0 +1,39 @@
+Integ. Pdi 2 
+public class while {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		
+		while (counter < 10) {
+			System.out.println(counter);
+			counter++;
+		}
+	}
+
+}
+
+public class for {
+
+	public static void main(String[] args) {
+		for(int counter=1;counter<=10;counter++) {
+			System.out.println(counter);
+		}
+
+	}
+
+}
+
+public class Stringbuilder {
+
+	public static void main(String[] args) {
+		
+		        String palindrome = "Racecar";
+		         
+		        StringBuilder sb = new StringBuilder(palindrome);
+		        
+		        sb.reverse();  // reverse it
+		        
+		        System.out.println(sb);
+		    }
+		}
+	
